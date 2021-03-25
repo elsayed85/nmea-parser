c08f6735c167dfabdfe1ad2ec2810dd8d998dd3f
unknown <elsayedkamal581999@gmail.com>
2021-03-25 22:56:35 +0200

    “wip”

--- src/Parser.php
+++ src/Parser.php
@@ -7,7 +7,7 @@ use \Exception;
 /**
  * Parse a nmea line to determine the frame type of the line and create an
  * instance of the frame type parser.
- * 
+ *
  * @package BultonFr\NMEA
  * @author Vermeulen Maxime <bulton.fr@gmail.com>
  */
@@ -18,38 +18,38 @@ class Parser
      * detected.
      */
     const ERR_FRAME_DETECT_FAILED = 10101;
-    
+
     /**
      * @const ERR_NO_PARSER_FOR_FRAME_TYPE Error code if there is not parser
      * for the readed line
      */
     const ERR_NO_PARSER_FOR_FRAME_TYPE = 10102;
-    
+
     /**
      * Read a new line, find the frame type, create a new instance of the frame
      * type parser and return the parser instanciated after reading and
      * parsing the line.
-     * 
+     *
      * @param string $line The line to parse
-     * 
+     *
      * @return \BultonFr\NMEA\Frame The frame type parser instance
      */
     public function readLine($line)
     {
         $frameType = $this->detectFrameType($line);
         $frame     = $this->obtainFrameParser($line, $frameType);
-        
+
         $frame->readFrame();
         return $frame;
     }
-    
+
     /**
      * Detect the frame type from the line
-     * 
+     *
      * @param string $line The line to read
-     * 
+     *
      * @return string
-     * 
+     *
      * @throws Exception If the line format not corresponding.
      */
     protected function detectFrameType($line)
@@ -61,31 +61,31 @@ class Parser
                 static::ERR_FRAME_DETECT_FAILED
             );
         }
-        
+
         return $matches[2]; //Always exist, else preg_match failed.
     }
-    
+
     /**
      * Instanciate the frame type class parser.
-     * 
+     *
      * @param string $line The line to read
      * @param string $frameType The frame type
-     * 
+     *
      * @return \BultonFr\NMEA\Frame
-     * 
+     *
      * @throws Exception If no parse exist for this frame type
      */
     protected function obtainFrameParser($line, $frameType)
     {
-        $frameClassName = '\BultonFr\NMEA\Frames\\'.$frameType;
-        
+        $frameClassName = '\BultonFr\NMEA\Frames\\' . $frameType;
+
         if (!class_exists($frameClassName)) {
             throw new Exception(
-                'There is no class defined for frame type '.$frameType.'.',
+                'There is no class defined for frame type ' . $frameType . '.',
                 static::ERR_NO_PARSER_FOR_FRAME_TYPE
             );
         }
-        
+
         return new $frameClassName($line);
     }
 }
